c140bda0aea35d4cf790a71153f17d8927fc030a
Kamil Mukhtarov <boybutterfly1@MacBook-Pro-Kamil.local>
2023-09-25 17:54:42 +0300

    Main page added

---
+++ src/pages/Main.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+
+</script>
+
+<template>
+  <div>
+    <button
+    @click="$router.push('/catalog')"
+    >
+      Catalog
+    </button>
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,13 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Catalog from '@/pages/Catalog.vue'
+import Main from '@/pages/Main.vue'
 
 const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'main',
+    component: Main
+  },
   {
     path: '/catalog',
     name: 'catalog',
