76bba420a40d5ea079382c8663adbb257c8c3d90
Kamil Mukhtarov <boybutterfly1@MacBook-Pro-Kamil.local>
2023-09-25 17:59:34 +0300

    ...

--- src/pages/Catalog.vue
+++ src/pages/Catalog.vue
@@ -1,10 +1,5 @@
 <script setup lang="ts">
 import FilmsList from '@/components/FilmsList.vue'
-import Pagination from '@/components/Pagination.vue'
-import { useFilmsStore } from '@/store/index'
-
-const filmsStore = useFilmsStore()
-
 
 </script>
 
